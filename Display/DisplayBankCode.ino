653d4147cb50c465968e0a2c82ae03a1fff68505
DavePearce <david.pearce@ecs.vuw.ac.nz>
2015-06-11 14:26:59 +1200

    Display: Adds code which runs

--- Display/DisplayBankCode.ino
+++ Display/DisplayBankCode.ino
@@ -1,7 +1,7 @@
 
-const int SER = 8;
-const int RCLK = 9;
-const int SRCLK = 10;
+const int SER = 2;
+const int RCLK = 1;
+const int SRCLK = 0;
 
 void setup() {
   pinMode(SER,OUTPUT);
@@ -10,31 +10,37 @@ void setup() {
 }
 
 byte digits[10] = {
-  0b11111100,  
-  0b01100000,
-  0b11011010,  
-  0b11110010,
-  0b01100110, 
-  0b10110110,
-  0b10111110,
-  0b11100000,
-  0b11111110,  
-  0b11110110,
+  0b11111100,  // 0
+  0b01100000,  // 1
+  0b11011010,  // 2
+  0b11110010,  // 3
+  0b01100110,  // 4
+  0b10110110,  // 5
+  0b10111110,  // 6
+  0b11100000,  // 7
+  0b11111110,  // 8
+  0b11110110,  // 9
 };
 
 void writeDigit(int display, int digit) {
    byte bits = digits[digit];
-   digitalWrite(RCLK,LOW);
-   // First, write the display selector
-   for(int i=1;i!=5;++i) {
+   digitalWrite(RCLK,LOW);   
+   // Second, write the display selector
+   for(int i=0;i!=4;++i) {
      digitalWrite(SRCLK,LOW);     
-     digitalWrite(SER,i == display ? LOW : HIGH);
+     digitalWrite(SER,i == display ? HIGH : LOW);
      digitalWrite(SRCLK,HIGH);     
    }
-   // Ssecond, write the segmenet selectors
+   // First, write dummy bit and col selectors
+   for(int i=0;i!=4;++i) {
+     digitalWrite(SRCLK,LOW);     
+     digitalWrite(SER,LOW);
+     digitalWrite(SRCLK,HIGH);         
+   }
+   // Third, write the segmenet selectors
    for(int i=0;i!=8;++i) {
      digitalWrite(SRCLK,LOW);
-     digitalWrite(SER,(bits&1) != 0 ? HIGH : LOW);
+     digitalWrite(SER,(bits&1) != 0 ? LOW : HIGH);
      digitalWrite(SRCLK,HIGH);
      bits = bits >> 1;
    }
@@ -48,12 +54,12 @@ int clock = 0;
 void loop() {
   // put your main code here, to run repeatedly:
   int tmp = counter;
-  for(int i=1;i!=5;++i) {
+  for(int i=0;i!=4;++i) {
     writeDigit(i,tmp % 10);  
     //delay(5);
     tmp = tmp / 10;
   }
-  clock = (clock + 1) % 1000;
+  clock = (clock + 1) % 50;
   if(clock == 0) {
     counter = counter + 1;
   }
